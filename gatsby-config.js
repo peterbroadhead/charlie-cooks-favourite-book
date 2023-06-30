766119066b5b53d77c9cdebf19c358b77f54456d
Peter Broadhead <p.t.broadhead@gmail.com>
2023-06-30 19:02:27 +0100

    Holding Page

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,10 +17,10 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Charlie Cook's Favourite Book`,
+    description: `Book your ticket for Charlie Cook's Favourite Book at The Little Angel Theatre now! `,
+    author: `Peter Broadhead`,
+    siteUrl: `https://charliecooksfavouritebook.com/`,
   },
   plugins: [
     {
@@ -53,7 +53,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/lat.png`, // This path is relative to the root of the site.
       },
     },
   ],
